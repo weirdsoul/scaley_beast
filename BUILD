f9b4e16d9201610b3c411fda714b199b6a7148f6
Andreas Eckleder <andreas@eckleder.de>
2017-04-15 12:18:35 +0200

    Some very basic setup files

---
+++ BUILD
@@ -0,0 +1,11 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_binary")
+load("@io_bazel_rules_go//go:def.bzl", "go_prefix")
+
+go_prefix("github.com/weirdsoul/browser_instruments")
+
+go_binary(
+	name = "instruments_server",
+	srcs = [
+	     "server/main.go",
+	],
+)
---
+++ server/main.go
@@ -0,0 +1,11 @@
+// The instruments server binary contains the server side code to receive
+// flight instrument data from UDP and export it via JSON.
+// It also serves the main html file and all Javascript.
+package main
+
+import "fmt"
+
+func main() {
+     fmt.Println("Instruments server - Version 0.1")
+     fmt.Println("Copyright 2017 Andreas Eckleder")
+}
