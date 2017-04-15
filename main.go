91e9f8b633f00e26d95cff89ea76a51e7c817aae
Andreas Eckleder <andreas@eckleder.de>
2017-04-15 12:44:21 +0200

    Changed go file

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ package main
 import "fmt"
 
 func main() {
-     fmt.Println("Instruments server - Version 0.1")
-     fmt.Println("Copyright 2017 Andreas Eckleder")
+	fmt.Println("Instruments server - Version 0.1")
+	fmt.Println("Copyright 2017 Andreas Eckleder")
 }
