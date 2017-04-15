249db45ee3e6f7379342bce283b29637aaccecfc
Andreas Eckleder <andreas@eckleder.de>
2017-04-15 16:17:58 +0200

    Some initial data from X-Plane.

--- main.go
+++ main.go
@@ -3,9 +3,72 @@
 // It also serves the main html file and all Javascript.
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+var port = flag.Int("port", 49042, "udp port to listen on")
+
+const receiverBufferSize = 2048
+
+// The type of control data contained in a data set.
+type ControlDataType int32
+
+// DataSet stores a single set of X-Plane control data.
+type DataSet struct {
+	// The index number of the data point.
+	Index ControlDataType
+	// The actual data values. Index determines their interpretation.
+	// Unused values contain either zero or -999.
+	Values [8]float32
+}
 
 func main() {
 	fmt.Println("Instruments server - Version 0.1")
-	fmt.Println("Copyright 2017 Andreas Eckleder")
+	fmt.Println("Copyright 2017 Andreas Eckleder\n")
+
+	flag.Parse()
+
+	log.Printf("Listening on port %v.\n", *port)
+
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%v", *port))
+	if err != nil {
+		log.Fatalf("net.ResolveUDPAddr: %v", err)
+	}
+	sock, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatalf("net.ListenUDP: %v", err)
+	}
+	// Main receiver loop. It never stops.
+	for {
+		buffer := make([]byte, receiverBufferSize)
+		n, senderAddr, err := sock.ReadFromUDP(buffer)
+		if err != nil {
+			log.Printf("sock.ReadFromUDP: %v", err)
+		}
+		log.Printf("Received %v bytes of data from %v", n, *senderAddr)
+		if (n-5)%36 == 0 && string(buffer[:4]) == "DATA" {
+			log.Printf("This looks like an X-Plane DATA packet.")
+			r := bytes.NewReader(buffer[5:n])
+			for {
+				var dataSet DataSet
+				if err = binary.Read(r, binary.LittleEndian, &dataSet); err != nil {
+					if err != io.EOF {
+						// Only print unexpected errors.
+						log.Printf("binary.Read: %v", err)
+					}
+					break
+				}
+				log.Printf("Message with index %v: %v", dataSet.Index, dataSet.Values)
+			}
+		} else {
+			log.Printf("Ignoring packet with header '%s'", string(buffer[:4]))
+		}
+	}
 }
