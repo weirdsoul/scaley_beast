54c41ad054b1bcb6b154b8f17611c3dc0bba7659
Andreas Eckleder <andreas@eckleder.de>
2017-04-30 17:22:18 +0200

    Going non-linear

--- scripts and docs/gauge.scm
+++ scripts and docs/gauge.scm
@@ -11,10 +11,10 @@
 ; starting at the topmost point of the circle.
 (define (circ centerX centerY radius deg)
   (let* (
-       (rad (* (/ (modulo (- 90 deg) 360) 360) 2 *pi*))
+       (rad (* (/ (- 90 deg) 360) 2 *pi*))
        (y (- centerY (* (sin rad) radius)))
        (x (+ centerX (* (cos rad) radius)))
-    )
+       )
     (list x y)
   )
 )
@@ -35,6 +35,13 @@
   )
 )
 
+; Maps the specified input to a polynomial. In the typical case, this is a non-linear
+; function to stretch part of the scale.
+(define (map-to-polynomial x)
+  (+ (* 1.194e-6 (expt x 4)) (* -0.00049614 (expt x 3))
+     (* 0.062352 (expt x 2)) (* -0.21389 x) -0.86754)
+)
+
 (define (script-fu-gauge inImage inLayer step evenMarkerLen oddMarkerLen padding)
   (let*
       (
@@ -42,11 +49,11 @@
        (yRes (car(gimp-drawable-width inLayer)))
        (centerX (/ xRes 2))
        (centerY (/ yRes 2))
-       (r (range 0 359 step))
+       (r (range 0 190 step))
        )
     (gimp-undo-push-group-start inImage)
     (while (not (null? r))
-	   (drawScale inLayer centerX centerY (car r)
+	   (drawScale inLayer centerX centerY (map-to-polynomial (car r))
 		      (if (> (modulo (car r) (* step 2)) 0) oddMarkerLen evenMarkerLen)
 		      padding)
 	   (set! r (cdr r))
@@ -62,11 +69,11 @@
     "Andreas Eckleder"                          ;author
     "copyright 2017, Andreas Eckleder"          ;copyright notice
     "April 30, 2017"                            ;date created
-    "RGB"                                      ;image type
+    "RGBA"                                      ;image type
     SF-IMAGE       "Image"         0
     SF-DRAWABLE    "Drawable"      0
-    SF-VALUE       "Step size"           "15"
-    SF-VALUE       "Even marker length"  "15"
+    SF-VALUE       "Step size"           "10"
+    SF-VALUE       "Even marker length"  "10"
     SF-VALUE       "Odd marker length"   "5"
     SF-VALUE       "Padding"             "50"
 )
