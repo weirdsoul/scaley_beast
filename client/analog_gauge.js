725a480d2c62586776b90110536148cb07019001
Andreas Eckleder <andreas@eckleder.de>
2017-05-21 22:57:19 +0200

    Altitude indicator alive

--- client/analog_gauge.js
+++ client/analog_gauge.js
@@ -26,7 +26,24 @@ browser_instruments.AnalogGauge = function(domElement) {
 	? parseInt(goog.dom.dataset.get(domElement, "source"), 10): 3;
     /** @type {number} **/
     this.inputIndex =  goog.dom.dataset.has(domElement, "index")
-	? parseInt(goog.dom.dataset.get(domElement, "index"), 10): 0;
+	? parseInt(goog.dom.dataset.get(domElement, "index"), 10): 0;    
+    if (goog.dom.dataset.has(domElement, "transform2")) {
+	// Data transform is used to convert the input signal x to degrees.
+	this.speedToDeg2 = new Function("x", goog.dom.dataset.get(domElement, "transform2"));
+    } else {
+	console.log("Using fallback function for input signal 2 conversion.");
+	/**Completely linear. Expects the input to be degrees.
+	 * @param {number} x Input to be converted.
+	 * @return {number} Resulting degrees for the gauge hand.
+	 */
+	this.speedToDeg2 = function(x) { return x; };
+    }
+    /** @type {number} **/
+    this.inputSource2 = goog.dom.dataset.has(domElement, "source2")
+	? parseInt(goog.dom.dataset.get(domElement, "source2"), 10): 3;
+    /** @type {number} **/
+    this.inputIndex2 =  goog.dom.dataset.has(domElement, "index2")
+	? parseInt(goog.dom.dataset.get(domElement, "index2"), 10): 0;
 };
 
 /**
@@ -34,12 +51,12 @@ browser_instruments.AnalogGauge = function(domElement) {
  * @param {!Object} msg A JSON object containing all the update information.
  */
 browser_instruments.AnalogGauge.prototype.updateInstrument = function(msg) {
-    if (msg["Index"] != this.inputSource) return;
-    var needleElement = goog.dom.getElementByClass("needle", this.domElement);
-    if (needleElement != null) {
-	/** @type {?Array<?Object>} **/
-	var valueSet = msg["Values"];
-	if (valueSet.length > 0) {
+    /** @type {?Array<?Object>} **/
+    var valueSet = msg["Values"];
+    if (valueSet.length == 0) return;    
+    if (msg["Index"] == this.inputSource) {
+	var needleElement = goog.dom.getElementByClass("needle", this.domElement);
+	if (needleElement != null) {
 	    var gaugeValue = parseFloat(valueSet[this.inputIndex]);
 	    if (gaugeValue != null) {
 		var degree = this.speedToDeg(gaugeValue);
@@ -48,9 +65,22 @@ browser_instruments.AnalogGauge.prototype.updateInstrument = function(msg) {
 					   "rotate(" + degree + " 240 240)");
 	    }
 	} else {
-	    console.error("Couldn't find value set.");
+	    console.error("Couldn't find needle element.");
 	}
-    } else {
-	console.error("Couldn't find needle element.");
-    }    
+    }
+    // The analog gauge supports two independent input sources. Not sure if we will ever
+    // need it, but this flexibility comes quite cheap. The second needle is purely
+    // optional, we don't even log when it is not there.
+    if (msg["Index"] == this.inputSource2) {
+	var needleElement2 = goog.dom.getElementByClass("needle2", this.domElement);
+	if (needleElement2 != null) {
+	    var gaugeValue = parseFloat(valueSet[this.inputIndex2]);
+	    if (gaugeValue != null) {
+		var degree = this.speedToDeg2(gaugeValue);
+		// TODO(aeckleder): Do not hardcode center point here.
+		needleElement2.setAttribute("transform",
+					    "rotate(" + degree + " 240 240)");
+	    }
+	}
+    }
 };
